748581ecc817f431e485d452c9f5ad4ed49c9496
muhmmad Nabh <muhammadnabih2020@gmail.com>
2023-09-16 04:02:27 +0300

    add some changes and new component its made navbar  and add it on main layout page to skip error from server about layout page because is use client on main layout page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
